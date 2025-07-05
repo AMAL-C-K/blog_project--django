020acef170e7684b0ec43fc8bcfea4c904ca4789
AMAL-C-K <amalckg1999@gmail.com>
2025-07-06 00:30:58 +0530

    Update settings.py

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+^@+5465gntsv4x)c)3ke4#&j7qhxt82isdrf37d^&*(9*6tuh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
